112d9f8eaa09d1a3b495977acfd73594c1bcfebf
egg899 <germanverissimo5@hotmail.com>
2022-04-07 22:01:18 -0400

    Created other ways to say Hello world

--- start/CH02/HelloWorld2.py
+++ start/CH02/HelloWorld2.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python with Inputs
-# Created 
+# Created Alberto German Verissimo on April 7, 2022
 
-# Suggestion, build out 1 line at a time
-# Once multiple print statemetns exist, put a breakpoint at first print line
-# Then walk through as an example of "debugging"
+your_name = input("What is your name?")
+print(your_name)
+print("Hello " + your_name)
+print("Hello", your_name)
+message = "Hello " + your_name
+print(message)
+print("Hello {0}".format(your_name))
+print(f"Hello {your_name}")
