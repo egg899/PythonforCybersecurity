70400cff3d2ff0b4a8161c05186b8446815fd53d
egg899 <germanverissimo5@hotmail.com>
2022-04-07 22:08:19 -0400

    Created the calculator

--- start/CH02/SimpleCalculator.py
+++ start/CH02/SimpleCalculator.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 # A simple calculator to show math and conditionals
-# Created 
+# Created by Alberto German Verissimo on April 7, 2022
+
+#Get information from user
+first_number =int(input("What is the first number? "))
+activity = input("What activity? (+ = * /) ")
+second_number = int(input("What is the second number? "))
+
+#Do math
+if activity == "+":
+    print(first_number + second_number)
+if activity == "-":
+    print(first_number - second_number)
+if activity == "*":
+    print(first_number * second_number)
+if activity == "/":
+    print(first_number / second_number)            
