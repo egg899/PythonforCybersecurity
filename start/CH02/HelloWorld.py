fcd80c45ccdf52e6cc215ec210a3e664ea92ffea
egg899 <germanverissimo5@hotmail.com>
2022-04-07 21:45:26 -0400

    Added the date to the Hello World File

--- start/CH02/HelloWorld.py
+++ start/CH02/HelloWorld.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python
-# Created by Alberto Verissimo on  April 7, 2022
+# Created by Alberto German Verissimo on  April 7, 2022
 
 print("Hello World")
