3bd7b7f4d921ad1c3e80bee87071f0acdb227d79
egg899 <germanverissimo5@hotmail.com>
2022-04-07 22:14:44 -0400

    Assignment 2

--- start/CH02/HelloWorld3.py
+++ start/CH02/HelloWorld3.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
 # A more complex "Hello World" script in python with Inputs
 # Created 
+name = input("What is your name? ")
+print("Hello {0}".format(name))
+print("Today is going to be a great day!")
+age = int(input("How old are you? "))
+print("In 2 years, you will be" , age + 2)
