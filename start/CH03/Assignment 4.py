b627611f97f4a58fb81e492fe33b63e579236f50
egg899 <germanverissimo5@hotmail.com>
2022-04-08 22:26:06 -0400

    Assignment 4 is done

---
+++ start/CH03/Assignment 4.py
@@ -0,0 +1,13 @@
+question = input("Is today a good day? (n/y)") 
+
+for i in range(10):
+  
+    if question == "y":
+        print("Yes, it is")
+
+
+
+# question = input("Is today a good day? (y/n)")
+# if question == "y":
+#     for i in range(10):
+#         print("Yes it is!")       
