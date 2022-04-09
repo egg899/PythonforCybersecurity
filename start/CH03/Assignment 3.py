4062955bb0d5b9e6e871c39bf8b7f72d85358cef
egg899 <germanverissimo5@hotmail.com>
2022-04-08 21:44:59 -0400

    Added conditionals

---
+++ start/CH03/Assignment 3.py
@@ -0,0 +1,10 @@
+#Write a script that:
+#Asks the user "Is today a good day? (y/n)"
+# Takes input from the user
+# If the answer is "y", print out "Yes it is"
+
+
+question = input("Is today a good day? (n/y)")
+
+if question == "y":
+    print("Yes, it is")
--- start/CH03/Conditionals.py
+++ start/CH03/Conditionals.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 # example workign with conditionals
-#By 
+#By Alberto German Verissimo on April 8, 2022
+
+x = 100
+y = 10
+
+if x == y:
+    print("x is equal to y")
+elif x < y:
+    print("x is less than y")
+else:
+    print("x is greater than y")        
+
+print("done")    
