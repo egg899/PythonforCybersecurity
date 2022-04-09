68ffd0922cbff7d615bc57e7ddda12bd02a5c41f
egg899 <germanverissimo5@hotmail.com>
2022-04-08 22:22:11 -0400

    Learned about loops

--- start/CH03/Loopy.py
+++ start/CH03/Loopy.py
@@ -1,3 +1,31 @@
 #!/usr/bin/env python3
 # example workign with Loops
-#By 
+#By Alberto German Verissimo on April 8, 2022
+
+# students =  ["Tom", "Sarah", "Bill", "Kate"]
+# for student in students:
+#     print("Hello {0}".format(student))
+#     print("  How are you today?")
+import os
+
+
+# exit = os.system("ping -c 1 -W 2 192.168.2.10 > /dev/null 2>&1")
+# print(exit)
+# for i in range(10):
+#     # print(i)
+#     if os.system(" ping -c 1 -W 2 192.168.2.{0} > /dev/null 2>&1".format(i + 1)) == 0:
+#         print("192.168.2.{0} > /dev/null 2>&1".format(i + 1))
+        
+
+
+# for i in range(10):
+#     print("192.168.0.{0}".format(i+1))      
+
+
+# i = 1
+# while i < 6:
+#     print(i)
+#     # i = i +1
+#     i+=1
+
+#     print("Done")
