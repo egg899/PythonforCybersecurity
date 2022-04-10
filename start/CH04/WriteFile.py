5587d3a050a8b6975f7a379fe76bf3cee78843d9
egg899 <germanverissimo5@hotmail.com>
2022-04-09 23:48:36 -0400

    WriteFile created and updated

--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 # Sample script that writes to a file
-# By 
+# By Alberto German Verissimo on April 8, 2022
+
+# Open file for writing
+test_file = open("testfile.txt", "w")
+
+#write lines to the file
+test_file.write("Hello World\n")
+test_file.write("My name is Alberto\n")
+test_file.write("I like rubber ducks\n")
+
+#Close the file
+test_file.close()
