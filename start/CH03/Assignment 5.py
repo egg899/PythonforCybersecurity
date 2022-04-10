8b9a81e80951006ec9f213171e9f735de586e897
egg899 <germanverissimo5@hotmail.com>
2022-04-09 23:35:09 -0400

    Assignment 5: Creating Functions

---
+++ start/CH03/Assignment 5.py
@@ -0,0 +1,28 @@
+# question = input("Is today a good day? (n/y)") 
+
+
+# def send_message(message):
+    
+#         if message == "y":
+#          print("Yes, it is")
+
+
+
+# for i in range(10):
+#     send_message(question)
+
+
+
+
+
+question = input("Is today a good day? (n/y)") 
+
+
+def send_message():
+    for i in range(10):
+        print("Yes, it is")
+
+
+
+    if question == "y":
+        send_message()    
