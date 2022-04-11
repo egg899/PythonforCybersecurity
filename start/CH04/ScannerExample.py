b12b69e96b47df69e4e181b183563c0f410faeea
egg899 <germanverissimo5@hotmail.com>
2022-04-11 18:11:14 -0400

    nmap scanner

--- start/CH04/ScannerExample.py
+++ start/CH04/ScannerExample.py
@@ -2,4 +2,32 @@
 # Port scanner example 
 # Use 'pip3 install python-nmap' to install modules
 # Use 'sudo apt -y install nmap' to install nmapccc
-# By 
+# By Alberto German Verisimo on Aprill 11, 2022
+
+# Import necessary Python modules
+import nmap
+
+#Idwntify target address
+target_address = "192.168.0.2"
+
+#Identify start and stop port for the scan
+port_start = 75
+port_end = 100
+
+#Create the scanner object
+scanner = nmap.PortScanner()
+
+print("Scanning {0}".format(target_address))
+
+#Loop through each por and scan
+for port in range(port_start, port_end + 1):
+    result = scanner.scan(target_address, str(port))
+    if len(result['scan']) == 0:
+        print('The port is closed')
+    #print(result['nmap']['scaninfo']['tcp']['services'])
+    #port_status = result['nmap']['scaninfo']['tcp']['services']
+    else:
+        port_status = result['scan'][target_address]['tcp'][port]['state']
+        print("\tPort: {0} is {1}".format(port, port_status))
+    
+   
