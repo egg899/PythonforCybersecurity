86d5e31ce769b5fcd83319fcee013bff0e1374f7
egg899 <germanverissimo5@hotmail.com>
2022-04-27 00:54:24 -0400

    Github Api

--- start/CH08/ListRepositories.py
+++ start/CH08/ListRepositories.py
@@ -5,22 +5,67 @@
 
 
 ##Let's get the authorization of the API
+# import requests
+
+# url = "https://api.github.com/user/repos"
+
+# payload={}
+# headers = {
+#   'Authorization': 'token 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'
+# }
+
+# # response = requests.request("GET", url, headers=headers, data=payload)
+
+# # print(response.text)
+
+
+# #Let's turn this response into a json object 
+# import json
+
+# def get_user():
+#     response = requests.request("GET", url, headers=headers, data=payload)
+#     # request_uri =response.text
+#     # r = requests.get(request_uri)
+#     items=response.json()
+#     return items
+
+# user = get_user()
+
+# for i in range(len(user)):
+    
+#     print(user[i]['id'])
+
+# # print(json.dumps(user, indent=2))
+
+
 import requests
+import configparser
+
+def get_api_key(key_name):
+  #Create configparser object and load file
+  config = configparser.ConfigParser()
+  config.read("/Users/user/Desktop/Programming and data analysis/Udemy/Cybersecurity/secrets.ini")
+  #Get the Api Key and return
+  api_key = config["APIkeys"][key_name]
+  return api_key
+
+#Get API key from file
+token = get_api_key('Github')
 
 url = "https://api.github.com/user/repos"
 
 payload={}
 headers = {
-  'Authorization': 'token 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'
+  'Authorization': 'token ' + token
 }
 
-# response = requests.request("GET", url, headers=headers, data=payload)
+
+# response = requests.request("GET", url, headers=headers, data =payload)
 
 # print(response.text)
 
-
-#Let's turn this response into a json object 
-import json
+# #Let's turn this response into a json object 
+# import json
 
 def get_user():
     response = requests.request("GET", url, headers=headers, data=payload)
@@ -29,10 +74,10 @@ def get_user():
     items=response.json()
     return items
 
+
 user = get_user()
-
+# print(user[0].keys())
 for i in range(len(user)):
-    
-    print(user[i]['id'])
-
-# print(json.dumps(user, indent=2))
+  print(user[i]['id'],'\n', user[i]['full_name'],'\n', user[i]['hooks_url'],'\n', user[i]['comments_url'],'\n','\n')
+  
+ 
