2cd0389ad7708bca0c21aa4a900dac519ec79a59
egg899 <germanverissimo5@hotmail.com>
2022-04-26 20:26:42 -0400

    Poeple in space

--- start/CH08/PeopleInSpace2.py
+++ start/CH08/PeopleInSpace2.py
@@ -1,3 +1,32 @@
 #!/usr/bin/env python3
 # Script that tells us how many people there are in space
-#By 
+#By Alberto German Verissimo on April 25, 2022
+
+#Import Python mdules
+import requests
+import json
+
+def get_people_in_space():
+    request_uri = "http://api.open-notify.org/astros.json"
+    r = requests.get(request_uri)
+    items = r.json()
+    return items
+
+astronauts = get_people_in_space()
+
+# print basic details    
+print(astronauts)    
+
+#print "pretty" json
+print(json.dumps(astronauts, indent=2))
+
+#search through datad to return specific information
+print("There are {0} people in space right now". format(astronauts["number"]))
+print("The first astronauts is {0} aboard the {1}".format(astronauts["people"][0]["name"], astronauts["people"][0]["craft"]))
+
+
+#Loop through all the people
+print("Full list of people in space")
+for person in astronauts["people"]:
+    print("{0} is aboard the {1}".format(person["name"], person["craft"]))
+
